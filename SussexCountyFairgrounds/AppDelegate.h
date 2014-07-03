97434e255c34c5b355d1570a299a60ac9cbe5c82
Administrator <admin@hpvh5bs.in.nam.ad.pwcinternal.com>
2014-07-03 11:52:49 +0530

    Initial Commit

---
+++ SussexCountyFairgrounds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SussexCountyFairgrounds
+//
+//  Created by Administrator on 7/3/14.
+//  Copyright (c) 2014 TCG S/W Inc.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SussexCountyFairgroundsTests/SussexCountyFairgroundsTests.m
@@ -0,0 +1,34 @@
+//
+//  SussexCountyFairgroundsTests.m
+//  SussexCountyFairgroundsTests
+//
+//  Created by Administrator on 7/3/14.
+//  Copyright (c) 2014 TCG S/W Inc.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SussexCountyFairgroundsTests : XCTestCase
+
+@end
+
+@implementation SussexCountyFairgroundsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
